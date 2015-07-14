8dd097a3424613baa2aece34183cba1af120db15
Rusty Wagner <rusty@vector35.com>
2015-07-14 18:29:09 -0400

    Add define to detect scc at compile time

--- runtime/defines.h
+++ runtime/defines.h
@@ -26,5 +26,7 @@
 #define offset_of(type, member) ((size_t)&(((type*)0)->member))
 #define container_of(ptr, type, member) ((type*)(((size_t)(ptr)) - offset_of(type, member)))
 
+#define __SCC__
+
 #endif
 

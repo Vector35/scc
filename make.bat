3fac9828b8bdadf9e63b79510e78a4873094bd3a
Rusty Wagner <rusty@vector35.com>
2016-03-08 17:37:44 -0500

    Use VS2015

--- make.bat
+++ make.bat
@@ -1,4 +1,4 @@
 @echo off
-call "c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
+call "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 set PATH=%~dp0\buildenv\msys\bin;%PATH%
 make.exe %*

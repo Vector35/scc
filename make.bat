28983e5a7edba4e42e62ce28d876345b64ec01ee
Rusty Wagner <rusty@vector35.com>
2020-05-26 22:16:11 -0400

    Update to VS2019

--- make.bat
+++ make.bat
@@ -1,4 +1,4 @@
 @echo off
-call "c:\Program Files (x86)\Microsoft Visual Studio\2017\WDExpress\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
 set PATH=%~dp0\buildenv\msys\bin;%PATH%
 make.exe %*

1c983733bc3c5b79561bbccbb7d296330bb8aba4
Peter LaFosse <peter@vector35.com>
2026-08-21 09:31:52 -0400

    Use the PowerPC socketcall ABI for networking (#628)

--- runtime/linux/ppc/syscall.h
+++ runtime/linux/ppc/syscall.h
@@ -376,5 +376,23 @@
 #define SYS_finit_module	353
 #define SYS_kcmp		354
 
-#endif
+// Operation numbers for the legacy socketcall multiplexer. These are distinct
+// from the direct socket syscall numbers above.
+#define SYS_SOCKET		  1
+#define SYS_BIND		  2
+#define SYS_CONNECT		  3
+#define SYS_LISTEN		  4
+#define SYS_ACCEPT		  5
+#define SYS_GETSOCKNAME	  6
+#define SYS_GETPEERNAME	  7
+#define SYS_SOCKETPAIR		  8
+#define SYS_SEND		  9
+#define SYS_RECV		 10
+#define SYS_SENDTO		 11
+#define SYS_RECVFROM		 12
+#define SYS_SHUTDOWN		 13
+#define SYS_SETSOCKOPT		 14
+#define SYS_GETSOCKOPT		 15
+#define SYS_ACCEPT4		 18
 
+#endif
